f8f353c2801a0ba8af918f10d310d8263903db85
jupaalmaa <114189026+jupaalmaa@users.noreply.github.com>
2024-08-03 12:50:13 -0400

    Created Payroll.java
    - Génération du code dans Modelio pour la classe Payroll.java

---
+++ projet/modelio/code-genere/Payroll.java
@@ -0,0 +1,17 @@
+import java.util.List;
+
+public class Payroll implements PayrollInterface{
+
+    public void imprimerPaie(List<PayInfo> infoPaie) {
+        //TODO Code a completer
+    }
+
+    public double netDeBrut(double grossPay) {
+        //TODO Code a completer
+        return 0; //valeur temporaire
+    }
+
+    public String rapportDeductions(List<PayInfo> infoPaie) {
+        return ""; //valeur temporaire
+    }
+}
